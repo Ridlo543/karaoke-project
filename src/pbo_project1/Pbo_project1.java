37d37f4df82951dc0bb72698e67ac134df628e58
Ridlo543 <ridloabdullahulinnuha543@gmail.com>
2023-12-10 00:30:26 +0700

    paket page dan media player

--- src/pbo_project1/Pbo_project1.java
+++ src/pbo_project1/Pbo_project1.java
@@ -15,6 +15,7 @@ public class Pbo_project1 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        new Paket().setVisible(true);
     }
     
 }
---
+++ src/pbo_project1/Ruangan.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pbo_project1;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class Ruangan {
+    private String nama;
+    private int kapasitas;
+    private int hargaPerJam;
+
+    public Ruangan(String nama, int kapasitas, int hargaPerJam) {
+        this.nama = nama;
+        this.kapasitas = kapasitas;
+        this.hargaPerJam = hargaPerJam;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public int getKapasitas() {
+        return kapasitas;
+    }
+
+    public int getHargaPerJam() {
+        return hargaPerJam;
+    }
+
+    @Override
+    public String toString() {
+        return nama + "\t" + kapasitas + " orang\t= " + hargaPerJam + "/jam";
+    }
+}
+
